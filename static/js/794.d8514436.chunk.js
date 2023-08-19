7f6485160d65bd871d6b4515cdcf549abdad10d8
Zhe1a <108029808+Zhe1a@users.noreply.github.com>
2023-08-19 10:58:41 +0000

    Deploying to gh-pages from @ Zhe1a/petly@1dc1011249debb03ced63f39f66febe85d525d15 🚀

---
+++ static/js/794.d8514436.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpetly=self.webpackChunkpetly||[]).push([[794],{1794:function(e,t,n){n.r(t);var o=n(3329),i=n(1087).NavLink;t.default=function(){return(0,o.jsxs)("div",{style:{marginTop:"100px",textAlign:"center"},children:[(0,o.jsx)("h1",{style:{marginBottom:"40px"},children:"Such page does not exist :("}),(0,o.jsx)(i,{to:"/",style:{color:"var(--accent)",fontSize:"20px",textDecoration:"underline"},children:"Go to homepage"})]})}}}]);
+//# sourceMappingURL=794.d8514436.chunk.js.map
