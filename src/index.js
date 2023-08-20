331b7fa829472dfacbdf706a4d3759ffc67fc93e
Євгеній <108029808+Zhe1a@users.noreply.github.com>
2023-08-20 14:57:53 +0300

    +

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/petly">
+      <BrowserRouter basename="/Zhe1a/petly">
         <App />
       </BrowserRouter>
     </PersistGate>
